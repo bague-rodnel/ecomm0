bda545de4578eb02e8859e5f8b2b97a680ba5725
Rodnel Bague <bague.rodnel@gmail.com>
2021-09-11 13:13:20 +0800

    footer fix

--- src/components/FeedingContainer.js
+++ src/components/FeedingContainer.js
@@ -10,9 +10,9 @@ export default function HereSection(){
 			<div className="row align-items-center justify-content-center">
 				<div className="col-md-4 col-12">
 					<h2 className="about-title">Feeding Them Right</h2>
-					<p className="about-text">Zuittzu Pet started with the core philosphy that "All pets are family." Our team's collective dream, is a world in which animal companions live long and healthy lives.</p>
+					<p className="about-text">Zuittzu started with the core philosphy that "All pets are family." Our team's collective dream, is a world in which animal companions live long and healthy lives.</p>
 					<p className="about-text">Together we strive for this goal, one get bowl at a time. Our dedicated nutritionists have spent hours crafting the besst and healthiest recipes for your pet's needs.</p>
-					<p className="about-text">Ony the best for your bestfriend. Zuittzu Pet.</p>
+					<p className="about-text">Ony the best for your bestfriend. Zuittzu.</p>
 				</div>
 				<div className="col-md-4 col-12">
 					<Cat />
